6bdb6816a94ae0b0dfd92dd789d9ae838d965f0c
Khaled Amr <desta@Khaleds-MacBook-Air.local>
2026-04-21 21:51:43 +0200

    Initial commit: project setup

---
+++ src/main/java/s55_19001057/t_01/Khaled_Amr/KhaledAmrApplication.java
@@ -0,0 +1,13 @@
+package s55_19001057.t_01.Khaled_Amr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhaledAmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhaledAmrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s55_19001057/t_01/Khaled_Amr/KhaledAmrApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_19001057.t_01.Khaled_Amr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhaledAmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
